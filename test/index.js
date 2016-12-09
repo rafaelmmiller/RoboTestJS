d331d049140be838e9872b77d980f2b5f2b7a908
Rafael <rafael.miller@usp.br>
2016-12-09 15:03:47 -0200

    yo node generated

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import roboTestJs from '../lib';
+
+describe('robo-test-js', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
